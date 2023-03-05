e923427a57ff2bf67c6485b88b4f8a9c30c50500
Matt <mjcampagna@gmail.com>
2023-03-04 16:52:52 -0800

    add Docusaurus to Git

---
+++ docusaurus/blog/2019-05-28-first-blog-post.md
@@ -0,0 +1,6 @@
+---
+slug: lorem-ipsum
+title: Lorem ipsum ...
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat.
---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,120 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Slithy Me',
+  // tagline: 'Dinosaurs are cool',
+  // favicon: 'img/favicon.ico',
+
+  baseUrl: '/', // the /<baseUrl>/ pathname under which your site is served
+  url: 'https://slithy.me', // production url of site
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        blog: {
+          showReadingTime: true,
+        },
+        docs: false,
+        // docs: {
+        //   sidebarPath: require.resolve('./sidebars.js'),
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Slithy.me',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
+        items: [
+          // {
+          //   docId: 'intro',
+          //   label: 'Docs',
+          //   position: 'left',
+          //   type: 'doc',
+          // },
+          {
+            label: 'Blog',
+            to: '/blog',
+            position: 'left'
+          },
+        ],
+      },
+      footer: {
+        // style: 'dark',
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'Tutorial',
+        //         to: '/docs/intro',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'Twitter',
+        //         href: 'https://twitter.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       {
+        //         label: 'Blog',
+        //         to: '/blog',
+        //       },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © ${new Date().getFullYear()} Matthew Campagna`,
+      },
+      prism: {
+        darkTheme: darkCodeTheme,
+        theme: lightCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
